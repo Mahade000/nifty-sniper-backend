de5a0b2a515fdeaba38a19cbeaa84d419c5e3e45
Mahade000 <alias.smelts.2j@icloud.com>
2025-07-23 02:08:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print('This is the main app file')
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Nifty Sniper 🔱 is LIVE!"
+
+@app.route('/getTradeSignal', methods=['POST'])
+def get_trade_signal():
+    return jsonify({"signal": "BUY", "target": 100, "sl": 95})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
